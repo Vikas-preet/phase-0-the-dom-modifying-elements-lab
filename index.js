233e57e4e16990920127698c4990417e477ded88
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-30 15:24:00 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id = "victory";
+//newHeader.setAttribute('id', 'victory')
+newHeader.textContent = "Vikas is the champion";
+// function championName (name) {
+// return
